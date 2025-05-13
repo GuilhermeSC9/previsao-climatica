9ae4f722206df8288619f6895bb4b89bc4b8dcce
Carvalho <70244224+GuilhermeSC9@users.noreply.github.com>
2025-05-12 21:46:35 -0300

    Finished: API Started: Frontend

--- src/routes/favoriteCities.ts
+++ src/routes/favoriteCities.ts
@@ -4,27 +4,27 @@ import { prisma } from '../lib/prisma'
 
 export async function favoriteCitiesRoutes(app: FastifyInstance) {
     //Adicionar cidades aos favoritos
-    app.post('/favorite-city', async (request, reply) =>{
+    app.post('/favoritecity', async (request, reply) =>{
         const createFavoriteCityBodySchema = z.object({
             name: z.string().min(3),
         })
 
         const { name } = createFavoriteCityBodySchema.parse(request.body)
 
-        const favoriteCity = await prisma.favoriteCity.findFirst({
+        const insertedCity = await prisma.favoriteCity.findFirst({
             where: {
                 name
             }
         })
 
-        if (favoriteCity){
+        if (insertedCity){
             return reply.status(409).send("Essa cidade já foi adicionada")
             
             
         } else{
             await prisma.favoriteCity.create({
                 data: {
-                    name
+                    name: name
                 }
             })
             return reply.status(201).send("Cidade adicionada com sucesso!")
@@ -32,7 +32,7 @@ export async function favoriteCitiesRoutes(app: FastifyInstance) {
 
     })
     //Remover cidades dos favoritos
-    app.delete('/favorite-city/:id', async(request, reply) =>{
+    app.delete('/favoritecity/:id', async(request, reply) =>{
         const deleteFavoriteCityParamsSchema = z.object({
             id: z.coerce.number(),
         })
@@ -40,21 +40,26 @@ export async function favoriteCitiesRoutes(app: FastifyInstance) {
         const { id } = deleteFavoriteCityParamsSchema.parse(request.params)
 
 
-        const favoriteCity = await prisma.favoriteCity.delete({
+        const insertedCity = await prisma.favoriteCity.findUnique({
             where: {
                 id
             }
         })
 
-        if(!favoriteCity){
+        if(!insertedCity){
             return reply.status(404).send('Cidade não encontrada')
             
-        } else {
-            return reply.status(204).send()
         }
+
+        await prisma.favoriteCity.delete({
+            where: {
+                id
+            }
+        })
+        return reply.status(204).send("Cidade removida")
     })
     //Buscar cidades favoritas
-    app.get("/favorite-city", async(request, reply) =>{
+    app.get("/favoritecity", async(request, reply) =>{
         const favoriteCities = await prisma.favoriteCity.findMany({})
 
         return reply.send({
--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -1,8 +1,41 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import { api } from '../lib/axios'
 
 export async function weatherRoutes(app: FastifyInstance) {
 	//Buscar tempo por cidade
-    //Buscar previsões para 5 dias
+    app.get("/weather/:cityName", async(request, reply) => {
+        const getCurrentWeatherParamsSchema = z.object({
+            cityName: z.string().min(1)
+        })
+
+        const { cityName } = getCurrentWeatherParamsSchema.parse(request.params)
+
+        const response = await api.get("" , {
+            params: {
+                q: cityName
+            }
+        })
+
+        return reply.send(response.data)
+    });
+
+    
+    //Buscar previsões para 3 dias
+    app.get("/weather/nextdays/:cityName", async(request, reply) =>{
+        const getNextDaysWeatherParamsSchema = z.object({
+            cityName: z.string().min(1)
+        })
+
+        const { cityName } = getNextDaysWeatherParamsSchema.parse(request.params);
+
+        const response = await api.get("" , {
+            params: {
+                q: cityName,
+                days: 3,
+            }
+        });
+        return reply.send(response.data)
+    })
 }
